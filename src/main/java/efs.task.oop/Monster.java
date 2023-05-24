98e1f68d24a24569ecad6290ec9a1f5d0270e4d7
Maks <109723200+mshestalyuk@users.noreply.github.com>
2023-05-24 11:40:46 +0200

    Update Monster.java

--- src/main/java/efs.task.oop/Monster.java
+++ src/main/java/efs.task.oop/Monster.java
@@ -1,11 +1,12 @@
 package efs.task.oop;
+
 public abstract class Monster implements Fighter  {
     protected int health;
     protected int damage;
     public int getDamage() {
         return damage;
     }
-    public int getHealth() {
+    protected int getHealth() {
         return health;
     }
     public Monster(int health, int damage) {
